fedb496f79c7880a154be40e9913b2bcaf335598
luxu <1115120651@qq.com>
2019-10-20 19:15:06 +0800

    路由

--- gateway/src/main/java/com/luxu/bootstrap/GatewayApplication.java
+++ gateway/src/main/java/com/luxu/bootstrap/GatewayApplication.java
@@ -2,7 +2,6 @@ package com.luxu.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @description:
@@ -11,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
