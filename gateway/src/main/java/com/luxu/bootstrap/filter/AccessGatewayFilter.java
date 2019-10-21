f006310efe6e53aadf6d4a5f043ca45bef5a8563
luxu <1115120651@qq.com>
2019-10-21 17:54:10 +0800

    完善网关模块

---
+++ gateway/src/main/java/com/luxu/bootstrap/filter/AccessGatewayFilter.java
@@ -0,0 +1,27 @@
+package com.luxu.bootstrap.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @description: 请求url权限校验
+ * @author: luxu
+ * @date: 2019-10-21 14:11
+ **/
+
+@Slf4j
+@Component
+public class AccessGatewayFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        return null;
+    }
+}
