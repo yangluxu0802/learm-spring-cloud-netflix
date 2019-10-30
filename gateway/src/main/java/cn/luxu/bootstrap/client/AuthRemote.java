b422313bf23b69f2ca7717351133b9ffe1df9cde
luxu <1115120651@qq.com>
2019-10-30 17:46:22 +0800

    整合网关与认证授权服务(未完成)

--- gateway/src/main/java/cn/luxu/bootstrap/client/AuthRemote.java
+++ gateway/src/main/java/cn/luxu/bootstrap/client/AuthRemote.java
@@ -1,36 +1,36 @@
-package cn.luxu.bootstrap.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-/**
- * @description:
- * @author: luxu
- * @date: 2019-10-28 15:25
- **/
-@FeignClient(name = "auth-service",fallback = AuthRemote.AuthRemoteFallback.class)
-public interface AuthRemote {
-
-    @RequestMapping(value = "/auth/ignore/{url}")
-    Boolean ignoreAuthentication(@PathVariable String url);
-
-    @PostMapping(value = "/auth/permission")
-    Boolean hasPermission(String authentication, String url, String method);
-
-    @Component
-    class AuthRemoteFallback implements AuthRemote {
-
-        @Override
-        public Boolean ignoreAuthentication(String url) {
-            return null;
-        }
-
-        @Override
-        public Boolean hasPermission(String authentication, String url, String method) {
-            return null;
-        }
-    }
-}
+//package cn.luxu.bootstrap.client;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+///**
+// * @description:
+// * @author: luxu
+// * @date: 2019-10-28 15:25
+// **/
+//@FeignClient(name = "auth-service",fallback = AuthRemote.AuthRemoteFallback.class)
+//public interface AuthRemote {
+//
+//    @RequestMapping(value = "/auth/ignore/{url}")
+//    Boolean ignoreAuthentication(@PathVariable String url);
+//
+//    @PostMapping(value = "/auth/permission")
+//    Boolean hasPermission(String authentication, String url, String method);
+//
+//    @Component
+//    class AuthRemoteFallback implements AuthRemote {
+//
+//        @Override
+//        public Boolean ignoreAuthentication(String url) {
+//            return null;
+//        }
+//
+//        @Override
+//        public Boolean hasPermission(String authentication, String url, String method) {
+//            return null;
+//        }
+//    }
+//}
